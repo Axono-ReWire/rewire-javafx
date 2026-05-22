ca450833e677ac19631eb95726f2aa483fff68c2
PlusUltrax <nifemiaina.a@gmail.com>
2026-05-22 21:21:13 +0100

    style: Checkstyle edits

--- src/main/java/com/axono/answer/ViewAnswerView.java
+++ src/main/java/com/axono/answer/ViewAnswerView.java
@@ -1,27 +1,39 @@
 package com.axono.answer;
 
 import javafx.application.Application;
-
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ViewAnswerView extends Application {
+/**
+ * JavaFX entry point used to launch the {@link AnswerView} in
+ * isolation with the canned test data from {@link TestAnswerView}.
+ */
+public final class ViewAnswerView extends Application {
+
+    /** Width of the test scene in pixels. */
+    private static final int SCENE_WIDTH = 900;
+
+    /** Height of the test scene in pixels. */
+    private static final int SCENE_HEIGHT = 900;
 
     @Override
-
-    public void start(Stage ansStage) {
+    public void start(final Stage ansStage) {
 
         TestAnswerView test = new TestAnswerView();
         AnswerView testAnswerView = new AnswerView(test.getQuestions());
 
-        Scene answerSceneTest = new Scene(testAnswerView, 900, 900);
+        Scene answerSceneTest = new Scene(testAnswerView, SCENE_WIDTH, SCENE_HEIGHT);
         ansStage.setScene(answerSceneTest);
         ansStage.setTitle("TEST");
         ansStage.show();
     }
 
-    public static void main(String[] args) {
+    /**
+     * JavaFX application launcher.
+     *
+     * @param args command-line arguments (unused)
+     */
+    public static void main(final String[] args) {
         launch(args);
     }
-
 }
---
+++ src/main/java/com/axono/questionmodel/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Domain model classes representing quiz questions and the user's
+ * selections for the Axono quiz application.
+ */
+package com.axono.questionmodel;
