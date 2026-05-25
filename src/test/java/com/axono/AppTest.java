3c0abecf1cb5db9d37975bbd3cd429a06516807f
Jenietha <mxl512@york.ac.uk>
2026-05-25 20:50:46 +0100

    test: created test class with sample test

---
+++ src/test/java/com/axono/AppTest.java
@@ -0,0 +1,14 @@
+package com.axono;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void sampleTest() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
