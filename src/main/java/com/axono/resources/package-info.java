43512be7cba922236ae8ee3c0c6e4d76541da8e5
swech145 <lswechha@gmail.com>
2026-06-01 12:13:08 +0100

    chore: adding package info file.

---
+++ src/main/java/com/axono/resources/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains resource view components for the application.
+ *
+ * Provides classes for displaying and navigating learning resources
+ * and module content within the application.
+ */
+package com.axono.resources;
